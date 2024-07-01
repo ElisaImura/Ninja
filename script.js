47fc220a79b0550e85e7d350233aaaeee2bbe987
ElisaImura <elisaimura45@gmail.com>
2024-07-01 10:57:46 -0400

    Sensei

--- script.js
+++ script.js
@@ -6,7 +6,33 @@ class Ninja {
         this.fuerza = 3;
     }
 
-    sayName = () =>console.log(`Nombre: ${this.nombre}`);
-    showStats = () => console.log(`Nombre: ${this.nombre}, Fuerza: ${this.fuerza}, Velocidad: ${this.velocidad}, Salud: ${this.salud}`);
-    drinkSake = () => this.salud+=10;
+    sayName() {
+        console.log(`Nombre: ${this.nombre}`);
+    }
+
+    showStats() {
+        console.log(`Nombre: ${this.nombre}, Fuerza: ${this.fuerza}, Velocidad: ${this.velocidad}, Salud: ${this.salud}`);
+    }
+
+    drinkSake() {
+        this.salud += 10;
+    }
 }
+
+class Sensei extends Ninja {
+    constructor(nombre){
+        super(nombre, 200);
+        this.fuerza = 10;
+        this.velocidad = 10;
+        this.sabiduria = 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom(); // Output: "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats(); // Output: "Nombre: Master Splinter, Fuerza: 10, Velocidad: 10, Salud: 210"
